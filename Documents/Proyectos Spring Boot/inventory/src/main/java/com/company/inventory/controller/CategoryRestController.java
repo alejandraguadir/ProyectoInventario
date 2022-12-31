41c58b6b39937ba6d7c38f5271436638cf05b47f
Alejandra Guadir <guadir.alejandra@gmail.com>
2022-12-31 11:26:37 -0500

    Categories full

--- Documents/Proyectos Spring Boot/inventory/src/main/java/com/company/inventory/controller/CategoryRestController.java
+++ Documents/Proyectos Spring Boot/inventory/src/main/java/com/company/inventory/controller/CategoryRestController.java
@@ -2,10 +2,16 @@ package com.company.inventory.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.company.inventory.model.Category;
 import com.company.inventory.response.CategoryResponseRest;
 import com.company.inventory.services.ICategoryService;
 
@@ -22,4 +28,31 @@ public class CategoryRestController {
 		
 	}
 
+	@GetMapping("/categories{id}")
+	public ResponseEntity<CategoryResponseRest> searchCategoriesById(@PathVariable Long id){
+		
+		ResponseEntity<CategoryResponseRest> response = service.searchById(id);
+		return response;
+		
+	}
+	@PostMapping("/categories")
+	public ResponseEntity<CategoryResponseRest> save(@RequestBody Category category){
+		ResponseEntity<CategoryResponseRest> response = service.save(category);
+		
+		return response;			
+	}
+	@PutMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@RequestBody Category category, @PathVariable Long id){
+		ResponseEntity<CategoryResponseRest> response = service.update(category, id);
+		
+		return response;
+		
+	}
+	@DeleteMapping("/categories/{id}")
+	public ResponseEntity<CategoryResponseRest> update(@PathVariable Long id){
+		ResponseEntity<CategoryResponseRest> response = service.deleteById(id);
+		
+		return response;
+		
+	}
 }
